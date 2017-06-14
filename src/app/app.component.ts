813035fe73bb2999ea7c8d62098fd1efad880a61
rtem92 <rtem92@gmail.com>
2017-06-14 19:43:48 +0300

    [Lesson 3] Provided navigation for app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
+// Added new class for navigation items elements
+export class NavItem {
+    name: string;
+    link: string;
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
+
 export class AppComponent {
-  title: string = 'Angular ITEA';
+    title: string = "Angular ITEA";
+    // Variable with navigation items
+    navItems: NavItem[] = [
+        { name: "Home", link: "Home" },
+        { name: "Catalog", link: "Catalog" },
+        { name: "Contact Us", link: "Contact-Us" }
+    ]
 }
