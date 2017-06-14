f1df6f727b8aa73ebd1109aa450778cbde1819cf
rtem92 <rtem92@gmail.com>
2017-06-14 19:21:40 +0300

    [Lesson 3] Added routes for new components

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
+import { CatalogComponent } from "./catalog/catalog.component";
+import { ContactUsComponent } from "./contact-us/contact-us.component";
 import { HomeComponent } from "./home/home.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
 // Define routes
 const ROUTES: Routes = [
     // { path: "name of path", component: "name of component class which will be binded to path" }
     { path: "", redirectTo: "Home", pathMatch: "full" },
-    { path: "Home", component: HomeComponent }
+    { path: "Home", component: HomeComponent },
+    // Added routes for new components
+    { path: "Catalog", component: CatalogComponent },
+    { path: "Contact-Us", component: ContactUsComponent },
+    // Umached path must be in the end of all routes
+    { path: "**", component: NotFoundComponent }
 ];
 
 @NgModule({
