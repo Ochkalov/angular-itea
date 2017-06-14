9e1be1b48afc0cfaecf4eaca5707ab6d5e140ca1
rtem92 <rtem92@gmail.com>
2017-06-14 20:44:40 +0300

    [Lesson 3] Applied styles to active routes + implemented page 404

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-not-found',
-  templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.css']
+  selector: "app-not-found",
+  templateUrl: "./not-found.component.html",
+  styleUrls: ["./not-found.component.css"]
 })
-export class NotFoundComponent implements OnInit {
+export class NotFoundComponent {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    goBack() {
+        window.history.back();    
+    }
 
 }
