e90abbe498b2a2430411356c73d734ebe62a8f90
rtem92 <rtem92@gmail.com>
2017-06-14 14:19:09 +0300

    [Lesson 2] Used HostListener decorator in bold.directive to listen user events

--- src/app/directives/bold.directive.ts
+++ src/app/directives/bold.directive.ts
@@ -1,14 +1,29 @@
-import {Directive, ElementRef, Input, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, Input, Renderer2} from "@angular/core";
  
 @Directive({
-    selector: '[bold]'
+    selector: "[bold]"
 })
 export class BoldDirective{
     // Used @Input decorator to get element which uses [bold]
-    @Input('bold') element: ElementRef;
+    @Input("bold") element: ElementRef;
 
     constructor(private elementRef: ElementRef, private renderer2: Renderer2){
-        // Set to element style with bold font
-        this.renderer2.setStyle(this.elementRef.nativeElement, "font-weight", "bold");
+        // Set to element style with pointer cursor
+        this.renderer2.setStyle(this.elementRef.nativeElement, "cursor", "pointer");
+    }
+    
+    // Used @HostListener decorator to listen event and call method
+    @HostListener("mouseenter") onMouseEnter() {
+        this.setFontWeight("bold");
+    }
+    
+    // Used @HostListener decorator to listen event and call method
+    @HostListener("mouseleave") onMouseLeave() {
+        this.setFontWeight("normal");
+    }
+    
+    // Method which provide setting of font-weight property in element
+    private setFontWeight(value: string) {
+        this.renderer2.setStyle(this.elementRef.nativeElement, "font-weight", value);
     }
 }
