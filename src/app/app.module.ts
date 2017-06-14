3e9bca7a060c7b81252d2717e340bac7e0524964
rtem92 <rtem92@gmail.com>
2017-06-14 15:13:06 +0300

    [Lesson 3] Created routing.module and used router-outled directive instead of component selector

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
+import { RoutingModule } from "./routing.module";
+
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
 
@@ -13,18 +15,19 @@ import { JoinPipe } from "./pipes/join.pipe";
 import { UsersService } from "./services/users.service";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    BoldDirective,
-    WhileDirective,
-    FactorialPipe,
-    JoinPipe
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [UsersService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        BoldDirective,
+        WhileDirective,
+        FactorialPipe,
+        JoinPipe
+    ],
+    imports: [
+        BrowserModule,
+        RoutingModule
+    ],
+    providers: [UsersService],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+
+import { HomeComponent } from "./home/home.component";
+
+// Define routes
+const ROUTES: Routes = [
+    // { path: "name of path", component: "name of component class which will be binded to path" }
+    { path: "", redirectTo: "Home", pathMatch: "full" },
+    { path: "Home", component: HomeComponent }
+];
+
+@NgModule({
+    "imports": [RouterModule.forRoot(ROUTES)],
+    "exports": [RouterModule]
+})
+
+export class RoutingModule { }
