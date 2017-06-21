ec93c7e3ec103d923dd6e9e8365c6f8ee9d9e206
Ochkalov <ochkalov@gmail.com>
2017-06-21 19:11:55 +0300

    5 lesson

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,32 +1,81 @@
-import { Component } from "@angular/core";
-// Import NgForm for using Form Object in template-drive forms
-import { NgForm } from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
+// Import FormBuilder and FormGroup modules for using data-driven forms
+// Import Validators class for validation
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 import { UsersService } from "../services/users.service";
 
 import { User } from "../models/user";
 
 @Component({
-    selector: "app-home",
-    templateUrl: "./home.component.html",
-    styleUrls: ["./home.component.css"]
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
-export class HomeComponent {
-    // define users variable
-    users: User[];
+export class HomeComponent implements OnInit {
+  // define variable to detect when form submitted
+  isFormSubmitted: boolean = false;
+  // define users variable
+  users: User[];
+  // define selectedUser var to edit existing user
+  selectedUser: User;
+  // define userForm variable as FormGroup
+  userForm: FormGroup;
+  // define RegExp variable to use in validators
+  REG_EXP: any = /^[\W]*([\w+\-.%]+@[\w\-.]+\.[A-Za-z]{2,4}[\W]*(;|,){1}[\W]*)*([\w+\-.%]+@[\w\-.]+\.[A-Za-z]{2,4})[\W]*$/;
 
-    constructor(
-        private userService: UsersService
-    ) {
-        this.users = this.userService.getUsers();
-    }
+  constructor(
+    // Add method which using FormBuilder class to build data-driven form
+    private formBuilder: FormBuilder,
+    private userService: UsersService
+  ) {
+    this.users = this.userService.getUsers();
+  }
 
-    // Method which will be called on form submit
-    onSubmit(e: Event, form: NgForm) {
-        // e.preventDefault() to disable of page reloading after submit
-        e.preventDefault();
-        // Added new user into users collection of user.service
-        this.userService.addUser(form.controls["firstName"].value, form.controls["lastName"].value, form.controls["email"].value, form.controls["age"].value);
+  // Method to clean validation on fields
+  clearControlValidation(name: string) {
+    this.userForm.controls[name].markAsTouched();
+  }
+
+  // Method to select existing user form list
+  onSelect(usr: User) {
+    this.selectedUser = usr;
+    this.userForm.controls["id"].setValue(this.selectedUser.id);
+    this.userForm.controls["firstName"].setValue(this.selectedUser.firstName);
+    this.userForm.controls["lastName"].setValue(this.selectedUser.lastName);
+    this.userForm.controls["email"].setValue(this.selectedUser.email);
+    this.userForm.controls["age"].setValue(this.selectedUser.age);
+  }
+
+  // Method which will be called on form submit
+  onSubmit(e: Event, form: FormGroup) {
+    this.isFormSubmitted = true;
+    // e.preventDefault() to disable of page reloading after submit
+    e.preventDefault();
+
+    // Mark controls as untouched to show error messages
+    this.userForm.controls["firstName"].markAsUntouched();
+    this.userForm.controls["lastName"].markAsUntouched();
+    this.userForm.controls["email"].markAsUntouched();
+
+    if (this.userForm.valid) {
+      // Added variable user as form value to push new user
+      let user: User = form.value;
+      this.userService.addUser(user);
+      this.userForm.reset();
+      this.isFormSubmitted = false;
     }
+  }
+
+  // Added building of data-driven form in OnInit method
+  ngOnInit() {
+    this.userForm = this.formBuilder.group({
+      id: [this.selectedUser ? this.selectedUser.id : null],
+      firstName: [this.selectedUser ? this.selectedUser.firstName : null, Validators.required],
+      lastName: [this.selectedUser ? this.selectedUser.lastName : null, Validators.required],
+      email: [this.selectedUser ? this.selectedUser.email : null, [Validators.required, Validators.pattern(this.REG_EXP)]],
+      age: [this.selectedUser ? this.selectedUser.age : null]
+    });
+  }
 
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -5,18 +5,36 @@ import { User } from "../models/user";
 @Injectable()
 export class UsersService {
 
-    private users: User[] = [
-        { firstName: "John", lastName: "Johnson", email: "jjohnson@mail.com", age: "32"},
-        { firstName: "Jack", lastName: "Jackson", email: "jjackson@mail.com", age: "25"},
-        { firstName: "Lois", lastName: "Lane", email: "llane@mail.com", age: "27"},
-        { firstName: "Kate", lastName: "King", email: "kking@mail.com", age: "30"}
-    ]
+  private users: User[] = [
+    { id: 1, firstName: "John", lastName: "Johnson", email: "jjohnson@mail.com", age: "32"},
+    { id: 2, firstName: "Jack", lastName: "Jackson", email: "jjackson@mail.com", age: "25"},
+    { id: 3, firstName: "Lois", lastName: "Lane", email: "llane@mail.com", age: "27"},
+    { id: 4, firstName: "Kate", lastName: "King", email: "kking@mail.com", age: "30"}
+  ]
 
-    getUsers(): User[] {
-        return this.users;
-    }
+  getUsers(): User[] {
+    return this.users;
+  }
 
-    addUser(firstName: string, lastName: string, email: string, age: string) {
-        this.users.push(new User(firstName, lastName, email, age));
+  addUser(user: User) {
+    let usr = user;
+    // If user has no id then add it to list
+    if (usr.id === undefined || usr.id === null) {
+      usr.id = this.users.length + 1;
+      // Change variable user to use adding without class constructor
+      this.users.push(usr);
+    } else {
+      // Call method to change existing user
+      this.editUser(usr);
     }
+  }
+
+  // Method which allows to edit existing user
+  editUser(user: User) {
+    let i: number = this.users.findIndex(item => item.id === user.id);
+
+    if (i !== -1) {
+      this.users[i] = user;
+    }
+  }
 }
